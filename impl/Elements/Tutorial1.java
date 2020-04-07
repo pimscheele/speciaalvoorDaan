c3728c8ed8c7554b9426afdd1dc70f28193a846a
Pim Scheele <pim.scheele@gmail.com>
2020-04-07 18:03:42 +0200

    Create Tutorial1.java

---
+++ impl/Elements/Tutorial1.java
@@ -0,0 +1,26 @@
+package impl.Elements;
+
+
+import behavior.behaviors.Collidable;
+import behavior.behaviors.KeyBehavior;
+import game.Element;
+
+import java.util.ArrayList;
+
+public class Tutorial1 extends Element implements Collidable, KeyBehavior {
+
+    
+    public Tutorial1() {
+        super("/resources/Tutorial1.0.jpeg");
+       
+    }
+	@Override
+	public void handleKeyPresses(ArrayList<String> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleCollision(Collidable collidable) {
+	}
+}
